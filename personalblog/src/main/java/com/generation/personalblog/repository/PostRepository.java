154aa2b68efd1b5e6b519b77b0901d730c315ad5
flavws <persona.fmaria@gmail.com>
2023-04-14 08:10:53 -0300

    repository adjustment

--- personalblog/src/main/java/com/generation/personalblog/repository/PostRepository.java
+++ personalblog/src/main/java/com/generation/personalblog/repository/PostRepository.java
@@ -2,7 +2,9 @@ package com.generation.personalblog.repository;
 
 import com.generation.personalblog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PostRepository extends JpaRepository<Post, Long>{
 
 }
